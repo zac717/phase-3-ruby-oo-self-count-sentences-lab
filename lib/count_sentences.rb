3d26eda2298f099a9bffa1dc482a87eb127fd374
Isaac Ongera <isaacongera717@gmail.com>
2023-02-19 10:40:56 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
 class String
+  # attr_reader :sentence
 
   def sentence?
-    
+    self.end_with?(".")
   end
+  
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.split(/[.!?]/).filter { |sentence| !sentence.empty? }.length
 
   end
 end
